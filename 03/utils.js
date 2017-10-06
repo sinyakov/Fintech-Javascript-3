7418ba89f1d6e47d82c809e803ec7b3c4eacfe6b
Max Sinyakov <max@sinyakov.com>
2017-10-06 21:16:19 +0300

    add 3.1 NumberAndString solution

--- 03/utils.js
+++ 03/utils.js
@@ -2,10 +2,7 @@ function delayPromise(promise, timeout) {
   let resolveValue = null;
   let rejectValue = null;
 
-  promise.then(
-    value => resolveValue = value,
-    err => rejectValue = err
-  );
+  promise.then(value => (resolveValue = value), err => (rejectValue = err));
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
