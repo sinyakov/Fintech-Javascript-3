a4ccafc0fda634f69be1e2aff8ff47dc182bafa6
Max Sinyakov <max@sinyakov.com>
2017-10-22 02:13:48 +0300

    feat: animated placeholder

--- 05/01/js/locality.js
+++ 05/01/js/locality.js
@@ -1,9 +1,9 @@
-const locality = document.querySelector('.form__item--locality');
+const locality = document.querySelector('.form__item-input--locality');
 
 locality.addEventListener('input', () => {
   if (locality.value.length > 10) {
-    locality.parentNode.classList.add('form__item-wrapper--error');
+    locality.parentNode.classList.add('form__item--error');
   } else {
-    locality.parentNode.classList.remove('form__item-wrapper--error');
+    locality.parentNode.classList.remove('form__item--error');
   }
 });
