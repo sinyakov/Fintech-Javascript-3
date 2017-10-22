2084c929fcbb9e6ba6868e110b0124ec71c2e9af
Max Sinyakov <max@sinyakov.com>
2017-10-22 04:36:08 +0300

    add locality validator

--- 05/01/js/locality.js
+++ 05/01/js/locality.js
@@ -1,7 +1,11 @@
 const locality = document.querySelector('.form__item-input--locality');
 
 locality.addEventListener('input', () => {
-  if (locality.value.length > 10) {
+  const isValid = !!locality.value.match(/^[а-яёА-ЯЁ0-9\-— ]*$/);
+  // в задании нужно проверять, что в названии нет латинских букв
+  // но, мне кажется, название в принципе может содержать только кириллические буквы, цифры и дефисы и тире
+
+  if (!isValid) {
     locality.parentNode.classList.add('form__item--error');
   } else {
     locality.parentNode.classList.remove('form__item--error');
