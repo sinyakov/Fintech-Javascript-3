7f38076732f4f787cacdf9138915da865094a443
Max Sinyakov <max@sinyakov.com>
2017-10-28 22:28:55 +0300

    use gulp to compile less

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const minifyCss = require('gulp-csso');
+const minifyJs = require('gulp-minify');
+const rename = require('gulp-rename');
+const rsync = require('gulp-rsync');
+
+const autoprefixer = require('autoprefixer');
+const del = require('del');
+const run = require('run-sequence');
+
+const server = require('browser-sync').create();
+
+gulp.task('clean', () => del('./build'));
+
+gulp.task('copy', () => gulp.src('./06/assets/**').pipe(gulp.dest('./build')));
+
+gulp.task('style', () =>
+  gulp
+    .src('./06/less/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(minifyCss())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./build'))
+    .pipe(server.stream())
+);
+
+gulp.task('compress', () =>
+  gulp
+    .src('./06/js/*.js')
+    .pipe(
+      minifyJs({
+        ext: {
+          min: '.js'
+        },
+        noSource: true
+      })
+    )
+    .pipe(gulp.dest('./build/js'))
+);
+
+gulp.task('build', done => run('clean', 'copy', 'style', 'compress', done));
+
+gulp.task('serve', () => {
+  server.init({
+    server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('./06/less/**.less', ['style']);
+  gulp.watch('./06/assets/**', ['copy']);
+});
+
+gulp.task('deploy', () =>
+  gulp.src('./build/**').pipe(
+    rsync({
+      root: 'build/',
+      username: 'root',
+      hostname: '188.226.171.191',
+      destination: '/var/www/tinkoff/',
+      recursive: true,
+      clean: true,
+      incremental: true,
+      exclude: '.DS_Store'
+    })
+  )
+);
